73bec12bc87564f154532d997704139d38d6bff2
Julio Quevedo <juliovquevedo@gmail.com>
2016-08-01 20:30:05 -0400

    added variables

--- client/madLib.js
+++ client/madLib.js
@@ -1,6 +1,17 @@
 Template.body.events({
 	'click #submitButton':function(event,template){
 		event.preventDefault();
-		console.log("Hello World");
-	}
+		
+		let exclamation = template.find("exclamation").value;
+		/*let adverb = template.find(#adverb).value;
+		let noun = template.find(#noun).value;
+		let verb = template.find(#verb).value;
+
+		let madlib = '/"' + exclamation + '!/" you say, as you ' + adverb + ' jump onto your ' +
+					 noun + ' and ' + verb + ', into the sunset/"';*/
+		Session.set('exclamation', exclamation);
+		console.log(exclamation);
+	},
+
 });
+
