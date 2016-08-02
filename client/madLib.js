6d533c4c1393cf4dc98a8e4423e2cd32957c70f8
Julio Quevedo <juliovquevedo@gmail.com>
2016-08-01 20:48:11 -0400

    final version

--- client/madLib.js
+++ client/madLib.js
@@ -9,6 +9,12 @@ Template.body.events({
     const adverb = template.find("input[name=adverb]").value;
     const noun = template.find("input[name=noun]").value;
     const verb = template.find("input[name=verb]").value;
-    console.log(`"${exclamation}", you say as you ${adverb} jump onto your ${noun} and ${verb} off into the sunset`);
+    
+    const text = `"${exclamation}!", you say as you ${adverb} jump onto your ${noun} and ${verb} off into the sunset`;
+  
+    $('#title').hide();
+    $('#title').html(text);
+    $('#title').css('color', '#0000ff');
+    $('#title').fadeIn('slow', function() {});
   },
 });
